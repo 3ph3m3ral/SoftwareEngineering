6ff9bd30dba674547e0d6a70595af2599d8fc49f
erma0x <ermano.buikis@gmail.com>
2022-01-27 14:50:23 +0100

    updates

--- security/anomization.sh
+++
@@ -1,8 +0,0 @@
-echo "STARTING ANONIMITY..."
-sudo openvpn open-vpn/OpenVPN/vpnbook-pl226-tcp443.ovpn
-sudo systemctl tor start
-sudo systemctl privoxy start
-
-sudo systemctl tor status
-sudo systemctl privoxy status
-echo "ANONIMITY COMPLETED"
